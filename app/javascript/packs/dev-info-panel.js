194c3d70c42edae67eb4c4fc67aae694adb80ae4
MVanderwood <m.b.vanderwood@gmail.com>
2020-08-07 08:33:44 -0600

    DevTools - Add a screen info box in development mode to layouts

---
+++ app/javascript/packs/dev-info-panel.js
@@ -0,0 +1,26 @@
+(() => {
+    window.onresize = () => {
+        document.querySelector('#view-width-value').textContent  = window.innerWidth;
+        document.querySelector('#view-height-value').textContent = window.innerHeight;
+    
+        const width = window.innerWidth
+        let screenType = document.querySelector('#screen-type-value');
+        let currentType = screenType.textContent;
+    
+        if(width > 1007) {
+            screenType.textContent = "Large Monitor";
+        } else if(width > 641) {
+            screenType.textContent = "Medium Monitor";
+        } else {
+            screenType.textContent = "Small Monitor";
+        }
+    
+        if (currentType != screenType.textContent) {
+            screenType.style.display = "none";
+            setTimeout(() => {
+                screenType.style.display = "inline";
+            }, 100);
+        }
+    };
+    window.onload = window.onresize;
+})()
